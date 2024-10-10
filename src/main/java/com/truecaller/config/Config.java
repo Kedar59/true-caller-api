425803e27f93415fa4b9660f6c88dce13fa489be
Kedar59 <kedarshidhaye2003@gmail.com>
2024-10-11 00:27:26 +0530

    -added swagger for documentation

--- src/main/java/com/truecaller/config/Config.java
+++ src/main/java/com/truecaller/config/Config.java
@@ -3,12 +3,17 @@ package com.truecaller.config;
 
 import com.truecaller.entities.Profile;
 import com.truecaller.services.ProfileService;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class Config {
-
+    @Bean
+    public OpenAPI myOpenAPI() {
+        return new OpenAPI();
+    }
 }
 
